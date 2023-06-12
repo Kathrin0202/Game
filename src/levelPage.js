5881d575157012ac56b77d60821d75484af0de55
Kathrin020 <katy-lola@mail.ru>
2023-06-12 22:40:09 +0300

    исправлен путь

--- src/levelPage.js
+++ src/levelPage.js
@@ -8,7 +8,7 @@ export function renderLevelPage(box, cardsCount) {
         const random = cards[Math.floor(Math.random() * cards.length)]
         openHtml += `
         <div data-index="${i}">
-        <img data-value="${random}"class="card" src='.src/css/asset/jpg/${random}.jpg' alt='${random}'/>
+        <img data-value="${random}"class="card" src='./static/asset/jpg/${random}.jpg' alt='${random}'/>
         </div>`
         cards.splice(cards.indexOf(random), 1)
     }
@@ -24,7 +24,7 @@ export function renderLevelPage(box, cardsCount) {
             </div>
         </div>
         <div class="time">
-            <time>00.00</time>
+            <time></time>
         </div>
     </div>
     <div class="startAgain">
@@ -37,14 +37,31 @@ export function renderLevelPage(box, cardsCount) {
     setTimeout(() => {
         const card = document.querySelectorAll('.card')
         card.forEach((car) => {
-            car.setAttribute('src', './jpg/рубашка.jpg')
+            car.setAttribute('src', './static/asset/jpg/рубашка.jpg')
         })
     }, 5000)
+    
+    let time = 0
+    let timer = setInterval(() => {
+    time++
 
+    const minutes = Math.floor(time / 60)
+        .toString()
+        .padStart(2, '0')
+    const seconds = (time % 60).toString().padStart(2, '0')
+    const gameTimer = document.querySelector('.time')
+    gameTimer.textContent = `${minutes}.${seconds}`
+    const buttonStart = document.querySelector('.buttonstart')
+    buttonStart.addEventListener('click', () => {
+        time = 0
+        gameTimer.textContent = '00.00'
+        clearInterval(timer)
+    })
+}, 1000)
     const card = document.querySelectorAll('.card')
     card.forEach((car) => {
         car.addEventListener('click', () => {
-            car.setAttribute('src', `./jpg/${car.dataset.value}.jpg`)
+            car.setAttribute('src', `./static/asset/jpg/${car.dataset.value}.jpg`)
             if (!firstCard) {
                 firstCard = car.dataset.value
             } else {
